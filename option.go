5e335a00aecb79333be1598125cea5aee7587a36
Raqbit <raqbit@outlook.com>
2021-03-20 19:13:10 +0100

    Add configurable options

---
+++ option.go
@@ -0,0 +1,37 @@
+package mcproto
+
+import (
+	"github.com/Raqbit/mcproto/packet"
+	"github.com/Raqbit/mcproto/types"
+)
+
+// Option is a configuration option for mcproto.
+type Option func(conn *connection)
+
+// WithLogger configures the logger for
+// mcproto.
+func WithLogger(logger Logger) Option {
+	return func(conn *connection) {
+		conn.logger = logger
+	}
+}
+
+// WithPackets allows you to add custom
+// packets to mcproto's packet registry.
+// This will make mcproto automatically parse
+// the packet when it is received.
+func WithPackets(packets []packet.Packet) Option {
+	return func(conn *connection) {
+		for _, pkt := range packets {
+			conn.registerPacket(pkt)
+		}
+	}
+}
+
+// WithSide configures the connection side
+// of the mcproto Connection.
+func WithSide(side types.Side) Option {
+	return func(conn *connection) {
+		conn.side = side
+	}
+}
